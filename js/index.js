5d66a28ae7e29f0b0d976cb8f412a6976ffb8254
02dejulho <lucianamts.blog@gmail.com>
2024-10-10 17:46:40 -0300

    Desenvolvendo Layout da aplicação, instalando TS e definindo funções para o elemento input

---
+++ js/index.js
@@ -0,0 +1,15 @@
+"use strict";
+//pegando o form e o input
+const form = document.querySelector('#search-form');
+const input = document.querySelector('#input-location');
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if (!input)
+        return;
+    const localizacao = input.value;
+    if (localizacao.length < 3) {
+        alert("Localização precisa ter pelo menos 3 caracteres");
+        return; //return evita que o código continue a roda enquanto o titulo da tarefa não tiver mais do que tres caracteres
+    }
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${localizacao}&appid=005f727e98cb13efdb1e91db1d26cf5a&lang=pt_br&units=metric`);
+});
